5f67c4ce22132f2da5e460b2202bf90f0f773c4b
Animesh Kumar <120721959+Animesh-X@users.noreply.github.com>
2024-12-19 17:29:31 +0530

    Add ABI and contract addresses for staking and stake token

---
+++ src/abi/constants.ts
@@ -0,0 +1,11 @@
+import Staking from "./staking.json";
+import StakeToken from "./stakeToken.json";
+import RewardToken from "./rewardToken.json";
+
+export const STAKING_ABI = Staking;
+export const STAKE_TOKEN_ABI = StakeToken;
+export const REWARD_TOKEN_ABI = RewardToken;
+
+export const STAKING_ADDRESS = "0x2420eE8F6725a9c4E3b0AD180d843F108241B712";
+export const STAKE_TOKEN_ADDRESS = "0x5FC6c63fe7A07C46512810b5fC615D94ff0678B4";
+export const REWARD_TOKEN_ADDRESS = "0x580FeeeEcc566410ff8189B71D00Cf4B9D7aa173";
